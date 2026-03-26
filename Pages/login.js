94a985954ef74b830dbaf017f1021f79401b6a66
Swamy <swamytech1@gmail.com>
2026-03-26 14:29:36 +0530

    2nd time commit

--- Pages/login.js
+++ Pages/login.js
@@ -4,18 +4,29 @@ export class LoginPage {
         this.page = page;
         this.usernameInput = page.locator('input[name="username"]');
         this.passwordInput = page.locator('input[name="password"]');
-        this.loginButton = page.getByRole('button', { name: 'Log In' });  
+        this.openLoginButton = page.getByRole('button', { name: 'Log In' });
+        this.loginButton = page.getByRole('button', { name: 'Log In' });
+        this.clickOnCattleFinanceButton = page.locator('button:has-text("CATTLE FINANCE")');
     }
 
     async gotoLoginPage() {
         await this.page.goto('https://aztest.feedlotmgr.com/');
     }
 
+    async clickLoginButton() {
+        await this.openLoginButton.click();
+    }
+
     async login(username, password) {
     await this.usernameInput.fill(username);
     await this.passwordInput.fill(password);
     await this.loginButton.click();
-    }   
+    }  
+    async clickCattleFinance() {
+        await this.clickOnCattleFinanceButton.click();
+    }
+
+
 
 }
 
